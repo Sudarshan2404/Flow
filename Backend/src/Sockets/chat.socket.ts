803015135d535871280030581297bf41ebeb56d5
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2026-04-18 15:35:03 +0530

    chat handler

--- Backend/src/Sockets/chat.socket.ts
+++ Backend/src/Sockets/chat.socket.ts
@@ -1,4 +1,17 @@
 import { Server, Socket } from "socket.io";
 import { Message } from "../models/messages.js";
 
-export const chatHandler = async (io: Server, socket: Socket) => {};
+export const chatHandler = async (io: Server, socket: Socket) => {
+  socket.on("send_message", async (senderId, conversationId, message, type) => {
+    if (type === "message") {
+      const msg = await Message.create({
+        conversationId,
+        senderId,
+        content: message,
+      });
+
+      io.to(conversationId).emit("recieve_msg", { payload: { senderId, msg } });
+      return;
+    }
+  });
+};
