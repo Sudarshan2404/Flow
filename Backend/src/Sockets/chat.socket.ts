decf957eb836e8613c3f3c3edc35bb10ffbf75ab
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2026-04-18 16:17:08 +0530

    made sockets more secure

--- Backend/src/Sockets/chat.socket.ts
+++ Backend/src/Sockets/chat.socket.ts
@@ -2,17 +2,27 @@ import { Server, Socket } from "socket.io";
 import { Message } from "../models/messages.js";
 
 export const chatHandler = async (io: Server, socket: Socket) => {
-  socket.on("send_message", async (conversationId, message, type) => {
-    const user = (socket as any).user;
-    if (type === "message") {
-      const msg = await Message.create({
-        conversationId,
-        senderId: user.id,
-        content: message,
-      });
+  socket.on("send_message", async (data) => {
+    try {
+      const user = (socket as any).user;
+      const { conversationId, type, message } = data;
+      if (type === "message") {
+        const msg = await Message.create({
+          conversationId,
+          senderId: user.id,
+          content: message,
+        });
 
-      io.to(conversationId).emit("recieve_msg", { payload: { user.id, msg } });
-      return;
+        io.to(conversationId).emit("recieve_msg", {
+          payload: { senderId: user.id, message: message },
+        });
+        return;
+      }
+    } catch (error) {
+      console.log("An Error Occured while sending message", error);
+      socket.emit("error_message", {
+        message: "Failed To send Message",
+      });
     }
   });
 };
--- Backend/src/Sockets/presence.socket.ts
+++ Backend/src/Sockets/presence.socket.ts
@@ -3,28 +3,43 @@ import { Server, Socket } from "socket.io";
 const onlineusers = new Map<string, Set<string>>();
 
 export const presenceHandler = (io: Server, socket: Socket) => {
-  socket.on("join", (userId: string) => {
-    socket.join(userId);
+  socket.on("join", () => {
+    try {
+      const user = (socket as any).user;
+      socket.join(user.id);
 
-    if (!onlineusers.has(userId)) {
-      onlineusers.set(userId, new Set<string>());
+      if (!onlineusers.has(user.id)) {
+        onlineusers.set(user.id, new Set<string>());
+      }
+
+      onlineusers.get(user.id)!.add(socket.id);
+      (socket as any).userId = user.id;
+    } catch (error) {
+      console.log("An error occured while joining socket", error);
+      socket.emit("error_message", {
+        message: "Failed to create a websocket connection",
+      });
     }
-
-    onlineusers.get(userId)!.add(socket.id);
-    (socket as any).userId = userId;
   });
 
   socket.on("disconnect", () => {
-    const userId = (socket as any).userId;
+    try {
+      const userId = (socket as any).user.id;
 
-    if (userId && onlineusers.has(userId)) {
-      const set = onlineusers.get(userId);
+      if (userId && onlineusers.has(userId)) {
+        const set = onlineusers.get(userId);
 
-      set?.delete(socket.id);
+        set?.delete(socket.id);
 
-      if (set?.size === 0) {
-        set?.delete(userId);
+        if (set?.size === 0) {
+          set?.delete(userId);
+        }
       }
+    } catch (error) {
+      console.log("An error occured while destroying socket", error);
+      socket.emit("error_message", {
+        message: "Failed to destroy a websocket connection",
+      });
     }
   });
 };
